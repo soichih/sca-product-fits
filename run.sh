314cf1a4a4da660f626b75f021995142389df0cb
Soichi Hayashi <soichih@gmail.com>
2016-06-23 00:18:00 +0000

    added -s for progress service posting

--- run.sh
+++ run.sh
@@ -18,10 +18,10 @@ echo $files
 for file in $files; do
     echo "processing $file"
 
-    curl -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to single extension fits\"}" $SCA_PROGRESS_URL
+    curl -s -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to single extension fits\"}" $SCA_PROGRESS_URL
     $SCA_SERVICE_DIR/mef2fits.py $file ../$input_task_id/$file
 
-    curl -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to png\"}" $SCA_PROGRESS_URL
+    curl -s -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to png\"}" $SCA_PROGRESS_URL
     $SCA_SERVICE_DIR/fits2img.py -t png -o $file.png $file
 
     echo "cleaning $file"
