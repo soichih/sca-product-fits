ecf0af0ecdcba6e35cae318afcb860bb26754ada
Soichi Hayashi <soichih@gmail.com>
2016-06-23 00:21:32 +0000

    updated the way progress is reported

--- run.sh
+++ run.sh
@@ -15,14 +15,22 @@ mkdir -p output
 files=`$SCA_SERVICE_DIR/jq -r '.[0] .files[] .filename' ../$input_task_id/products.json`
 echo $files
 
+#report to progress service before we begin
+i=0
 for file in $files; do
-    echo "processing $file"
+    ((i = i + 1))  
+    curl -s -X POST -H "Content-Type: application/json" -d "{\"name\":\"$file\", \"status\":\"waiting\"}" ${SCA_PROGRESS_URL}.$i
+done
 
-    curl -s -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to single extension fits\"}" $SCA_PROGRESS_URL
+i=0
+for file in $files; do
+    ((i = i + 1))  
+
+    curl -s -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to single extension fits\", \"status\":\"running\"}" ${SCA_PROGRESS_URL}.$i
     $SCA_SERVICE_DIR/mef2fits.py $file ../$input_task_id/$file
 
-    curl -s -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to png\"}" $SCA_PROGRESS_URL
-    $SCA_SERVICE_DIR/fits2img.py -t png -o $file.png $file
+    curl -s -X POST -H "Content-Type: application/json" -d "{\"msg\":\"converting $file to png\"}" ${SCA_PROGRESS_URL}.$i
+    $SCA_SERVICE_DIR/fits2img.py -t png -o ${file}.png $file
 
     echo "cleaning $file"
     rm $file
