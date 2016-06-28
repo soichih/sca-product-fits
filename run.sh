1ec1c4137b6d08580169b036ed49c42a764905b0
Soichi Hayashi <soichih@gmail.com>
2016-06-28 00:08:29 +0000

    fixed typo

--- run.sh
+++ run.sh
@@ -28,6 +28,7 @@ for file in $files; do
 
     curl -s -X POST -H "Content-Type: application/json" \
         -d "{\"msg\":\"converting $file to single extension fits\", \"status\":\"running\"}" ${SCA_PROGRESS_URL}.$i
+
     echo "$SCA_SERVICE_DIR/mef2fits.py $file ../$input_task_id/$file"
     $SCA_SERVICE_DIR/mef2fits.py $file ../$input_task_id/$file
     if [ ! $? -eq 0 ]; then
@@ -38,20 +39,23 @@ for file in $files; do
 
     curl -s -X POST -H "Content-Type: application/json" \
         -d "{\"msg\":\"converting $file to png\", \"progress\":0.5}" ${SCA_PROGRESS_URL}.$i
+
     echo "$SCA_SERVICE_DIR/fits2img.py -t png -o $file $file"
     $SCA_SERVICE_DIR/fits2img.py -t png -o $file $file
     if [ ! $? -eq 0 ]; then
         curl -s -X POST -H "Content-Type: application/json" \
             -d "{\"msg\":\"fits2img returned $?\", \"status\":\"failed\"}" ${SCA_PROGRESS_URL}.$i
+        continue 
     fi
 
     curl -s -X POST -H "Content-Type: application/json" \
         -d "{\"msg\":\"denerating DZI\", \"progress\":0.8}" ${SCA_PROGRESS_URL}.$i
-    echo "$SCA_SERVICE_DIR/png2dzi ${file}.png ${file}.dzi"
-    $SCA_SERVICE_DIR/png2dzi ${file}.png ${file}.dzi
+    echo "$SCA_SERVICE_DIR/png2dzi.py ${file}.png ${file}.dzi"
+    $SCA_SERVICE_DIR/png2dzi.py ${file}.png ${file}.dzi
     if [ ! $? -eq 0 ]; then
         curl -s -X POST -H "Content-Type: application/json" \
             -d "{\"msg\":\"png2dzi returned $?\", \"status\":\"failed\"}" ${SCA_PROGRESS_URL}.$i
+        continue 
     fi
 
     curl -s -X POST -H "Content-Type: application/json" \
